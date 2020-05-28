93dccbfcf9c7e8d7887dbf8c17ad13c8b89bab48
mikaljuren <63348364+mikaljuren@users.noreply.github.com>
2020-05-28 19:27:20 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+
+// Question 1 & 2
+
+var petArray = ["cat", "cow", "dog"];
+console.log(petArray[1]);
+
+// Question 3
+
+var petArray = ["cat" "cow", "dog"];
+petArray.push("sheep");
+
+// Question 4
+
+var petArray = ["cat", "cow", "dog"];
+var numberOfPets = petArray.length;
+console.log(numberOfPets);
+
+// Question 5
+
+var catObject = {
+    name: "Catfish",
+    color: "Orange",
+    age: 4
+};
+
+
+//Question 6
+
+console.log(catObject.age);
+
+// Question 7
+
+var catArray = [
+    {
+        name: "Catfish",
+        color: "Orange",
+        age: 4
+    }
+];
+
+// Question 8
+
+for(var i = 0; i < catArray.length; i++) {
+
+console.log(catArray[i]);
+}
+
+// Question 9
+
+function logToConsole(myArgument) {
+    console.log(myArgument);
+}
+
+// Question 10
+
+function logToConsole(myArgument) {
+    console.log(myArgument);
+}
+
+myArgument("donkey");
+
+
